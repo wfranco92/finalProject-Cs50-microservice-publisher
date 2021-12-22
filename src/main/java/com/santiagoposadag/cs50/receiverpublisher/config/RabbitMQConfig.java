363c559e64d1e33352ea420ace7e38dd1ab81ef9
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-12-22 17:13:16 -0500

    Finished rabbitMQ class configuration

--- src/main/java/com/santiagoposadag/cs50/receiverpublisher/config/RabbitMQConfig.java
+++ src/main/java/com/santiagoposadag/cs50/receiverpublisher/config/RabbitMQConfig.java
@@ -1,2 +1,74 @@
-package com.santiagoposadag.cs50.receiverpublisher.config;public class RabbitConfig {
+package com.santiagoposadag.cs50.receiverpublisher.config;
+
+
+
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String GENERAL_QUEUE = "action.general";
+    public static final String SELL_QUEUE = "action.sell";
+    public static final String BUY_QUEUE = "action.buy";
+
+    public static final String EXCHANGE = "actions_exchange";
+    public static final String GENERAL_ROUTING_KEY = "routingKey.*";
+    public static final String SELL_ROUTING_KEY = "routingKey.sell";
+    public static final String BUY_ROUTING_KEY = "routingKey.buy";
+
+    @Bean
+    public Queue getGeneralQueue() {
+        return new Queue(GENERAL_QUEUE);
+    }
+
+    @Bean
+    public Queue getSellQueue() {
+        return new Queue(SELL_QUEUE);
+    }
+
+    @Bean
+    public Queue getBuyQueue() {
+        return new Queue(BUY_QUEUE);
+    }
+
+    @Bean
+    public TopicExchange getTopicExchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding BindingToGeneralQueue() {
+        return BindingBuilder.bind(getGeneralQueue()).to(getTopicExchange()).with(GENERAL_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding BindingToSellQueue() {
+        return BindingBuilder.bind(getSellQueue()).to(getTopicExchange()).with(SELL_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding BindingToBuyQueue() {
+        return BindingBuilder.bind(getBuyQueue()).to(getTopicExchange()).with(BUY_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter getMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+
+    }
+
+    @Bean
+    public AmqpTemplate getAmqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(getMessageConverter());
+        return rabbitTemplate;
+    }
+
 }
