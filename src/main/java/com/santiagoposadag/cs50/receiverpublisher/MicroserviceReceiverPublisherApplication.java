9571d6804d227f147e5d0607c628f1754a010b9f
santiagoposadag <santiago90dell@gmail.com>
2021-12-22 15:37:27 -0500

    Initialoced project

---
+++ src/main/java/com/santiagoposadag/cs50/receiverpublisher/MicroserviceReceiverPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.santiagoposadag.cs50.receiverpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceReceiverPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceReceiverPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santiagoposadag/cs50/receiverpublisher/MicroserviceReceiverPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiagoposadag.cs50.receiverpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceReceiverPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
